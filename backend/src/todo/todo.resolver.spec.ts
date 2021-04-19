98cd173c0fb877d2aa758c03b8faa87eeea37500
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-04-19 13:57:50 +0200

    findOne test

--- backend/src/todo/todo.resolver.spec.ts
+++ backend/src/todo/todo.resolver.spec.ts
@@ -21,5 +21,14 @@ describe('TodoResolver', () =>{
 
       await expect( resolver.findAll()).resolves.toBe(result);
     });
+  });
+
+  describe('findOne', () => {
+    it('should return one todo', async () => {
+      const result = {id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null };
+      jest.spyOn(todoService, 'findOne').mockImplementation(() => Promise.resolve(result))
+
+      await expect( resolver.findOne("1")).resolves.toBe(result);
+    });
   })
 })
