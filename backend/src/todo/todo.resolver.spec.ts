a2a6fb8cd1177969d00f462bcc407c0f71440803
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-04-19 13:55:09 +0200

    findAll test

---
+++ backend/src/todo/todo.resolver.spec.ts
@@ -0,0 +1,25 @@
+import { TodoService } from './todo.service';
+import { PrismaService } from '../services/prisma.service';
+import { Todo } from '../../dist/todo/entities/todo.entity';
+import { TodoResolver } from './todo.resolver';
+
+describe('TodoResolver', () =>{
+  let todoService: TodoService;
+  let prismaService: PrismaService
+  let resolver: TodoResolver
+
+  beforeEach( () => {
+    prismaService = new PrismaService()
+    todoService = new TodoService(prismaService);
+    resolver = new TodoResolver(todoService);
+  });
+
+  describe('findAll', () => {
+    it('should return an array of todos', async () => {
+      const result = [{id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null }];
+      jest.spyOn(todoService, 'findAll').mockImplementation(() => Promise.resolve(result));
+
+      await expect( resolver.findAll()).resolves.toBe(result);
+    });
+  })
+})
