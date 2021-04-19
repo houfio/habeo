6d62618ee77e3299af7729abf2df019802a06ce8
Rick van den Heuvel <Rick.heuvel@outlook.com>
2021-04-19 10:53:57 +0200

    added prisma client

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaServiceService } from './prisma-service/prisma-service.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, PrismaServiceService, PrismaService]
 })
 export class AppModule {
 }
