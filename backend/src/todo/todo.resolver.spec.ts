b90d7f9ef9b761f0ab7b8d089eb714a3fe1af6d4
houfio <26674115+houfio@users.noreply.github.com>
2021-04-19 14:13:46 +0200

    Add prepare script

--- backend/src/todo/todo.resolver.spec.ts
+++ backend/src/todo/todo.resolver.spec.ts
@@ -1,53 +1,50 @@
-import { TodoService } from './todo.service';
 import { PrismaService } from '../services/prisma.service';
-import { Todo } from '../../dist/todo/entities/todo.entity';
+
+import { TodoService } from './todo.service';
 import { TodoResolver } from './todo.resolver';
 
-describe('TodoResolver', () =>{
+describe('TodoResolver', () => {
   let todoService: TodoService;
-  let prismaService: PrismaService
-  let resolver: TodoResolver
+  let resolver: TodoResolver;
 
-  beforeEach( () => {
-    prismaService = new PrismaService()
-    todoService = new TodoService(prismaService);
+  beforeEach(() => {
+    todoService = new TodoService(new PrismaService());
     resolver = new TodoResolver(todoService);
   });
 
   describe('findAll', () => {
     it('should return an array of todos', async () => {
-      const result = [{id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null }];
+      const result = [{ id: 1, title: 'title', text: 'text', icon: 'icon', completedAt: null }];
       jest.spyOn(todoService, 'findAll').mockImplementation(() => Promise.resolve(result));
 
-      await expect( resolver.findAll()).resolves.toBe(result);
+      await expect(resolver.findAll()).resolves.toBe(result);
     });
   });
 
   describe('findOne', () => {
     it('should return one todo', async () => {
-      const result = {id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null };
-      jest.spyOn(todoService, 'findOne').mockImplementation(() => Promise.resolve(result))
+      const result = { id: 1, title: 'title', text: 'text', icon: 'icon', completedAt: null };
+      jest.spyOn(todoService, 'findOne').mockImplementation(() => Promise.resolve(result));
 
-      await expect( resolver.findOne("1")).resolves.toBe(result);
+      await expect(resolver.findOne('1')).resolves.toBe(result);
     });
   });
 
   describe('create', () => {
     it('should create a todo', async () => {
-      const result = {id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null };
-      jest.spyOn(todoService, 'create').mockImplementation(() => Promise.resolve(result))
+      const result = { id: 1, title: 'title', text: 'text', icon: 'icon', completedAt: null };
+      jest.spyOn(todoService, 'create').mockImplementation(() => Promise.resolve(result));
 
-      await expect(resolver.create({title: 'title', text: 'text', icon:'icon'})).resolves.toBe(result);
+      await expect(resolver.create({ title: 'title', text: 'text', icon: 'icon' })).resolves.toBe(result);
     });
   });
 
   describe('remove', () => {
-    it('should remove todo', async () => {
-      const result = {id: 1,title: 'title', text: 'text', icon:'icon', completedAt: null };
-      jest.spyOn(todoService, 'remove').mockImplementation(() => Promise.resolve(result))
+    it('should remove a todo', async () => {
+      const result = { id: 1, title: 'title', text: 'text', icon: 'icon', completedAt: null };
+      jest.spyOn(todoService, 'remove').mockImplementation(() => Promise.resolve(result));
 
-      await expect(resolver.remove("1")).resolves.toBe(result);
+      await expect(resolver.remove('1')).resolves.toBe(result);
     });
   });
-
-})
+});
