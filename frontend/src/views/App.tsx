fe4ed84b48dcc3e04e0301a9bc9c5cc6ef36901b
houfio <26674115+houfio@users.noreply.github.com>
2021-04-19 12:05:54 +0200

    Add input to app

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,20 +1,43 @@
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 
 import { useStore } from '../composables/useStore';
 import { Container } from '../components/Container';
+import { Input } from '../components/Input';
 
 import styles from './App.module.scss';
 
 export default defineComponent({
-  setup() {
+  setup: () => {
     const { state, commit } = useStore();
+    const todo = reactive({
+      text: ''
+    });
+
+    const onSubmit = (e: Event) => {
+      e.preventDefault();
+
+      const trimmed = todo.text.trim();
+
+      if (!trimmed) {
+        return;
+      }
+
+      commit('addItem', {
+        text: trimmed,
+        done: false
+      });
+      todo.text = '';
+    };
 
     return () => (
       <Container>
-        <div>
-          <button onClick={() => commit('addItem', { text: 'Todo', done: false })}>
-            Add
-          </button>
+        <div class={styles.header}>
+          <h1 class={styles.title}>
+            To-do List
+          </h1>
+          <form class={styles.form} onSubmit={onSubmit}>
+            <Input v-model={todo.text}/>
+          </form>
         </div>
         {state.todos.map((todo, i) => (
           <div key={i}>
