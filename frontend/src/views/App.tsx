36e9f7f3cd6242dc8d4f33d797291c8c274d9f2e
houfio <26674115+houfio@users.noreply.github.com>
2021-04-19 10:36:57 +0200

    Add store actions

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,10 +1,21 @@
 import { defineComponent } from 'vue';
 
+import { useStore } from '../composables/useStore';
+
 export default defineComponent({
   setup() {
+    const { state, commit } = useStore();
+
     return () => (
       <div>
-        Habeo
+        <button onClick={() => commit('addItem', { text: 'Todo', done: false })}>
+          Add
+        </button>
+        {state.todos.map((todo, i) => (
+          <div key={i}>
+            {todo.text}
+          </div>
+        ))}
       </div>
     );
   }
